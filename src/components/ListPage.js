91a42581b77861a2ae4d7909c839ee4aea5bdcc9
robdel12 <robertdeluca19@gmail.com>
2017-01-31 20:23:21 -0600

    Implement impagination.js

--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -1,13 +1,72 @@
 import React from 'react';
 import Photo from '../components/presentational/Photo';
 import Loading from './presentational/Loading';
+import Infinite from 'react-infinite';
+import Dataset from 'impagination';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+
+const ITEM_HEIGHT = 600;
+const HEADER_HEIGHT = 80;
+const PAGE_SIZE = 2;
 
 class ListPage extends React.Component {
 
   static propTypes = {
     data: React.PropTypes.object,
+  }
+
+  state = {
+    dataset: null,
+    datasetState: null,
+  }
+
+  setupImpagination() {
+    let _this = this;
+
+    let dataset = new Dataset({
+      pageSize: PAGE_SIZE,
+      loadHorizon: PAGE_SIZE * 2,
+
+      // Anytime there's a new state emitted, we want to set that on
+      // the componets local state.
+      observe(datasetState) {
+        _this.setState({datasetState});
+      },
+
+      // Where to fetch the data from.
+      fetch(pageOffset, pageSize, stats) {
+        let skip = pageOffset * pageSize;
+
+        console.group('skip + pagesize');
+        console.log('skip:', skip);
+        console.log('pageSize:', pageSize);
+        console.groupEnd();
+
+        return _this.props.data.fetchMore({
+          variables: {
+            skip: skip,
+            first: pageSize
+          },
+          updateQuery: (prev, { fetchMoreResult }) => {
+            return;
+          }
+        }).then(response => response.data.allPhotos);
+      }
+    });
+
+    dataset.setReadOffset(0);
+    this.setState({dataset});
+  }
+
+  componentWillMount() {
+    this.setupImpagination();
+  }
+
+  setCurrentReadOffset = (event) => {
+    let currentItemIndex = Math.ceil((window.scrollY - HEADER_HEIGHT) / ITEM_HEIGHT);
+
+    this.state.dataset.setReadOffset(currentItemIndex);
   }
 
   render () {
@@ -16,16 +75,22 @@ class ListPage extends React.Component {
     }
     return (
       <div style={{maxWidth: "600px", margin: "0 auto", padding: "20px 0"}}>
-        {this.props.data.allPhotos.map(photo =>
-          <Photo key={photo.id} photo={photo} user={photo.user} />
-        )}
+        <Infinite elementHeight={ITEM_HEIGHT} handleScroll={this.setCurrentReadOffset} useWindowAsScrollContainer>
+          {this.state.datasetState.map(record => {
+            if ((record.isPending && !record.isSettled)) {
+              return <div key={Math.random()} style={{height: "600px"}}>Loading...</div>;
+            }
+
+            return (<Photo key={record.content.id} photo={record.content} user={record.content.user} />);
+          })}
+        </Infinite>
       </div>
     );
   }
 }
 
-const FeedQuery = gql`query {
-  allPhotos(orderBy: createdAt_DESC) {
+const FeedQuery = gql`query($skip: Int!, $first: Int!) {
+  allPhotos(orderBy: createdAt_DESC, first: $first, skip: $skip) {
     id,
     imageUrl,
     description,
@@ -39,4 +104,4 @@ const FeedQuery = gql`query {
   }
 }`;
 
-export default graphql(FeedQuery)(ListPage);
+export default graphql(FeedQuery, {options: {variables: { skip: 0, first: PAGE_SIZE }}})(ListPage);
