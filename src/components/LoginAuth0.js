64ebde3886f5b5a41a5cdb2e891923e680a80938
robdel12 <robertdeluca19@gmail.com>
2017-01-31 20:53:19 -0600

    Update to .env to be deployable

--- src/components/LoginAuth0.js
+++ src/components/LoginAuth0.js
@@ -2,17 +2,12 @@ import React, { Component, PropTypes } from 'react';
 import Auth0Lock from 'auth0-lock';
 import { withRouter } from 'react-router';
 
-import {
-  AUTH_0_CLIENT_ID,
-  AUTH_0_DOMAIN
-} from '../env';
-
 class LoginAuth0 extends Component {
 
   constructor (props) {
     super(props);
 
-    this._lock = new Auth0Lock(AUTH_0_CLIENT_ID, AUTH_0_DOMAIN);
+    this._lock = new Auth0Lock(process.env.REACT_APP_AUTH_0_CLIENT_ID, process.env.REACT_APP_AUTH_0_DOMAIN);
   }
 
   static propTypes = {
